572f9d5dc629eabfe61974e3b900e4b0d03cd4a7
abchapagain <achyutchapagain05@gmail.com>
2023-05-27 07:26:14 +0545

    day 2

---
+++ Controllers/ClassInfoController.cs
@@ -0,0 +1,101 @@
+using FirstApp.DataContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class ClassInfoController : Controller
+{
+
+    private readonly AppDbContext _context;
+    public ClassInfoController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var list = await _context.ClassInfos.ToListAsync();
+        return View(list);
+    }
+
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View(new ClassInfoVm());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ClassInfoVm model)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new Exception("Invalid Data");
+            }
+
+            var classInfo = new ClassInfo()
+            {
+                Name = model.Name,
+                Description = model.Description,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _context.ClassInfos.AddAsync(classInfo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+
+    [HttpGet]
+
+    public async Task<IActionResult> Update(long id)
+    {
+        try
+        {
+
+
+            var info = await _context.ClassInfos.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (info == null) throw new Exception($"class info with id {id} not found");
+
+            var model = new ClassInfoVm()
+            {
+                Name = info.Name,
+                Description = info.Description
+            };
+            return View(model);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(int id, ClassInfoVm model)
+    {
+        try
+        {
+            var info = await _context.ClassInfos.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (info == null) throw new Exception($"class info with id {id} not found");
+
+            info.Name = model.Name;
+            info.Description = model.Description;
+
+            _context.ClassInfos.Update(info);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+}
---
+++ Models/ClassInfoVm.cs
@@ -0,0 +1,7 @@
+public class ClassInfoVm
+{
+    public string Name { get; set; }
+
+    public string? Description { get; set; }
+
+}
